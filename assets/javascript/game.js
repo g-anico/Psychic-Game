fef4c9d9e3c826350ad8d41b2b9ec7b8a8f6bd28
Anco Chien <anco.chien@gmail.com>
2017-05-13 01:38:09 -0700

    removed css

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+// I need an array of letters a-z.
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Computer needs to randomly pick a letter
+
+
+// I need an event listener for user's letter choice
+// See if it matches computer's guess
+// if user's guess equals computer's guess 
+	//Win counter goes up by one - use innerHTML
+
+	// 
+// game has to be reset without refreshing the page
